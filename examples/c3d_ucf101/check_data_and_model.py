95a93d25f26ff8440cbbdc5e5819736dd15ef520
Chuck Cho <chuck.cho@dextro.co>
2017-09-25 11:30:59 -0400

    WIP for demo

--- examples/c3d_ucf101/check_data_and_model.py
+++ examples/c3d_ucf101/check_data_and_model.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# set up Python environment: numpy for numerical routines, and matplotlib for plotting
-import numpy as np
-import matplotlib.pyplot as plt
-
 # The caffe module needs to be on the Python path;
 #  we'll add it here explicitly.
 import sys
@@ -11,10 +7,14 @@ caffe_root = '../../'  # this file should be run from {caffe_root}/examples (oth
 sys.path.insert(0, caffe_root + 'python')
 
 import caffe
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 # If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
 
-import os
-model_weights = './c3d_ucf101_iter_5000.caffemodel'
+model_weights = './c3d_ucf101_iter_20000.caffemodel'
 if not os.path.isfile(model_weights):
     print "[Error] model weights can't be found."
     sys.exit(-1)
@@ -23,6 +23,26 @@ print 'model found.'
 caffe.set_mode_gpu()
 caffe.set_device(1)
 model_def = './c3d_ucf101_deploy.prototxt'
+
+classes = [
+    "ApplyEyeMakeup", "ApplyLipstick", "Archery", "BabyCrawling", "BalanceBeam", "BandMarching",
+    "BaseballPitch", "Basketball", "BasketballDunk", "BenchPress", "Biking", "Billiards",
+    "BlowDryHair", "BlowingCandles", "BodyWeightSquats", "Bowling", "BoxingPunchingBag",
+    "BoxingSpeedBag", "BreastStroke", "BrushingTeeth", "CleanAndJerk", "CliffDiving",
+    "CricketBowling", "CricketShot", "CuttingInKitchen", "Diving", "Drumming", "Fencing",
+    "FieldHockeyPenalty", "FloorGymnastics", "FrisbeeCatch", "FrontCrawl", "GolfSwing",
+    "Haircut", "HammerThrow", "Hammering", "HandstandPushups", "HandstandWalking",
+    "HeadMassage", "HighJump", "HorseRace", "HorseRiding", "HulaHoop", "IceDancing",
+    "JavelinThrow", "JugglingBalls", "JumpRope", "JumpingJack", "Kayaking", "Knitting",
+    "LongJump", "Lunges", "MilitaryParade", "Mixing", "MoppingFloor", "Nunchucks", "ParallelBars",
+    "PizzaTossing", "PlayingCello", "PlayingDaf", "PlayingDhol", "PlayingFlute", "PlayingGuitar",
+    "PlayingPiano", "PlayingSitar", "PlayingTabla", "PlayingViolin", "PoleVault", "PommelHorse",
+    "PullUps", "Punch", "PushUps", "Rafting", "RockClimbingIndoor", "RopeClimbing", "Rowing",
+    "SalsaSpin", "ShavingBeard", "Shotput", "SkateBoarding", "Skiing", "Skijet", "SkyDiving",
+    "SoccerJuggling", "SoccerPenalty", "StillRings", "SumoWrestling", "Surfing", "Swing",
+    "TableTennisShot", "TaiChi", "TennisSwing", "ThrowDiscus", "TrampolineJumping", "Typing",
+    "UnevenBars", "VolleyballSpiking", "WalkingWithDog", "WallPushups", "WritingOnBoard", "YoYo"
+]
 
 net = caffe.Net(model_def,      # defines the structure of the model
                 model_weights,  # contains the trained weights
@@ -44,16 +64,25 @@ transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
 transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
 
 # set the size of the input (we can skip this if we're happy
-#  with the default; we can also change it later, e.g., for different batch sizes)
-net.blobs['data'].reshape(1,        # batch size
+# with the default; we can also change it later, e.g., for different batch sizes)
+net.blobs['data'].reshape(1,         # batch size
                           3,         # 3-channel (BGR) images
-                          length,        # length of a clip
+                          length,    # length of a clip
                           112, 112)  # image size
 
-clip = np.tile(
-        caffe.io.load_image(caffe_root + 'examples/images/cat.jpg'),
-        (16,1,1,1)
-        )
+#frame_files = sorted(glob.glob(caffe_root + 'examples/videos/youtube_objects_dog_v0002_s006/*.jpg'))
+frame_files = sorted(glob.glob(caffe_root + 'examples/videos/UCF-101_Rowing_g16_c03/*.jpg'))[:16]
+
+frame_img = caffe.io.load_image(frame_files[0])
+clip = np.empty((16,)+ frame_img.shape, dtype=np.uint8)
+for frame_num, frame_file in enumerate(frame_files):
+    frame_img = caffe.io.load_image(frame_file)
+    clip[frame_num,:,:,:] = frame_img
+
+#clip = np.tile(
+#       caffe.io.load_image(caffe_root + 'examples/images/cat.jpg'),
+#        (16,1,1,1)
+#        )
 clip = np.transpose(clip, (1,2,3,0))
 print "clip.shape={}".format(clip.shape)
 
@@ -74,4 +103,4 @@ output = net.forward()
 
 output_prob = output['prob'][0]  # the output probability vector for the first image in the batch
 
-print 'predicted class is:', output_prob.argmax()
+print 'predicted class is:', classes[output_prob.argmax()]
